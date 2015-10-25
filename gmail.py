26c232e01b02867bd4c4d3b0a39ba7cb216e46c0
Ravi Patel <ravi.patel@bazaarvoice.com>
2015-10-25 03:27:35 -0500

    --read-file option works

--- gmail.py
+++ gmail.py
@@ -4,6 +4,7 @@ import os
 import re
 from fuzzywuzzy import process
 from apiclient import discovery
+from apiclient import errors
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
@@ -23,12 +24,24 @@ import sys
 #   - Thread exception when reading ALL emails
 #   - No warning when no parameters are specified
 #   - Custom labels will have issues with finding them.
+#   - disabled subject by default
+# Nice to have
+#   - Comments
+#   - Cleaner code
+# Documentation
+#   - SPAM label wont delete
+#   - Default Labels
+#   - How to mark action in the file
+# Ideas
+#   - include_subject by default?
+#   - if count only list, then don't include message_id and query for all data
 #*********************************************
 email_list = []
 label_map = {}
 
 class Email_Object:
-    def __init__(self, email_from, subject):
+    def __init__(self, message_id, email_from, subject):
+        self.message_id = message_id
         self.email_from = email_from
         self.subject = subject
 
@@ -95,21 +108,60 @@ class appendList (threading.Thread):
                         email_sub = (header['value'])
 
                 if from_who and email_sub:
-                    email_list.append(Email_Object(from_who, email_sub))
+                    email_list.append(Email_Object(message_id['id'], from_who, email_sub))
                 elif from_who:
-                    email_list.append(Email_Object(from_who, ''))
+                    email_list.append(Email_Object(message_id['id'], from_who, ''))
                 elif email_sub:
-                    email_list.append(Email_Object('', email_sub))
+                    email_list.append(Email_Object(message_id['id'], '', email_sub))
 
                 # print("*********")
         
-def CreateMsgLabels():
+def create_msg_labels(add_list, remove_list):
   """Create object to update labels.
 
   Returns:
     A label update object.
   """
-  return {'removeLabelIds': [], 'addLabelIds': ['Junk']}
+  return {'addLabelIds': add_list, 'removeLabelIds': remove_list}
+
+def modify_message_labels(service, user_id, msg_id, msg_labels):
+  """Modify the Labels on the given Message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: The id of the message required.
+    msg_labels: The change in labels.
+
+  Returns:
+    Modified message, containing updated labelIds, id and threadId.
+  """
+  try:
+    message = service.users().messages().modify(userId=user_id, id=msg_id,
+                                                body=msg_labels).execute()
+
+    print ('Label changes for message with id: %s: ' % msg_labels)
+
+    return message
+  except errors.HttpError, error:
+    print ('An error occurred: %s' % error)
+
+
+def delete_message(service, user_id, message_id):
+  """Delete a Message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: ID of Message to delete.
+  """
+  try:
+    service.users().messages().delete(userId=user_id, id=message_id).execute()
+    print ('Message with id: %s deleted successfully.' % message_id)
+  except errors.HttpError, error:
+    print ('An error occurred: %s' % error)
 
 def create_label_mapping(service, user_id):
     results = service.users().labels().list(userId=user_id).execute()
@@ -181,7 +233,7 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
     input_file = ''
-    columns = ["Email", "Count", "Action"]
+    columns = ["Message ID", "Email", "Count", "Action"]
 
     create_label_mapping(service, user_id)
 
@@ -190,25 +242,37 @@ def main():
 
     if args.read_file:
         print ('read file: %s' % args.read_file)
-        readFile=csv.reader(open(args.read_file,'rb'), delimiter=',')
-        # message = service.users().messages().modify(userId=user_id, id='1507f6f154951151',
-                                                # body=CreateMsgLabels()).execute()
+        readFile=csv.reader(open(args.read_file,'rU'), delimiter=',')
+        
         for line in readFile:
-            email=line[0]
-            action=line[2]
-            subject=line[3]
-            search_query="from:"+email+' and subject:' + subject
+            message_id=line[0]
+            email=line[1]
+            action=line[3]
+            subject=line[4]
+            add_label_list = []
+            remove_label_list = []
+
+            search_query = 'from:' + email + ' and subject:' + subject
+
+            if email == "Email": #its the first line
+                continue
 
             if action == 'delete':
-                print ("delete message with query" + search_query)
+                delete_message(service, user_id, message_id)
             elif 'label' in action:
-                print('Change label')
+                for label_action in action.split(';'):
+                    if 'add-label' in label_action:
+                        for user_label in label_action.split(':')[1].split(' '):
+                            add_label_list.append(label_map[user_label]) 
+                    if 'remove-label' in label_action:
+                        for user_label in label_action.split(':')[1].split(' '):
+                            remove_label_list.append(label_map[user_label]) 
+                modify_message_labels(service, user_id, message_id, create_msg_labels(add_label_list, remove_label_list))
             else:
                 print('No action specified for:')
                 print(' email: ' + email)
                 print(' subject: ' + subject)
                 print('') #Visual presentation
-
     else:
         if args.output == 'csv':
             writer = CsvOutputWriter(columns, args.file_name)
@@ -238,7 +302,7 @@ def main():
 
         if args.include_subject:
             for entry in email_list:
-                writer.write(entry.email_from, '1', '', entry.subject.encode('utf-8','ignore'))
+                writer.write(entry.message_id, entry.email_from, '1', '', entry.subject.encode('utf-8','ignore'))
         else:
             counted_emails = collections.Counter(i.email_from for i in email_list)
             for entry in counted_emails.keys():
