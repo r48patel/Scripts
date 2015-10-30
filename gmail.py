ccc2f3be42d0fd070a784ac69407d7e008f0d063
Ravi Patel <ravi.patel@bazaarvoice.com>
2015-10-30 01:48:46 -0500

    KeyboardInterrupt handeled properly and some documentation

--- gmail.py
+++ gmail.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+# from __future__ import print_function
 import httplib2
 import os
 import re
@@ -15,6 +15,7 @@ import time
 import csv
 from prettytable import PrettyTable
 import sys
+import signal
 import argparse
 import threading
 import sys
@@ -24,18 +25,19 @@ import sys
 #   - Thread exception when reading ALL emails
 #   - No warning when no parameters are specified
 #   - Custom labels will have issues with finding them.
-#   - disabled subject by default
 # Nice to have
 #   - Comments
 #   - Cleaner code
+#   - Creat a label if given doesn't exists
 # Documentation
 #   - SPAM label wont delete
 #   - Default Labels
 #   - How to mark action in the file
+#   - queries example
 # Ideas
-#   - include_subject by default?
 #   - if count only list, then don't include message_id and query for all data
 #*********************************************
+
 email_list = []
 label_map = {}
 
@@ -81,10 +83,11 @@ class CsvOutputWriter(OutputWriter):
         print(self.file.name + ' written.')
         self.file.close()
 
-class appendList (threading.Thread):
+class processEmails (threading.Thread):
     def __init__(self, user_id, message_list):
         threading.Thread.__init__(self)
         self.user_id = user_id
+        # Each thread has its own service so it doesn't interfere with others.
         self.service = discovery.build('gmail', 'v1', http=get_credentials().authorize(httplib2.Http()))
         self.message_list = message_list
     
@@ -93,31 +96,39 @@ class appendList (threading.Thread):
 
         if self.message_list:
             for message_id in self.message_list:
-                # print("*********")
-                # print(message_id['id'])
-                headers = self.service.users().messages().get(userId=self.user_id, id=message_id['id']).execute()['payload']['headers']
-                for header in headers:
-                    # print(header)
-                    if header['name'] == 'From':
-                        try:
-                            from_who = (re.findall(r'(<.*>)', header['value'])[0].replace("<", "").replace(">", ""))
-                        except IndexError:
-                            from_who = (header['value'])
+                try:
+                    headers = self.service.users().messages().get(userId=self.user_id, id=message_id['id']).execute()['payload']['headers']
+                    from_who = None
+                    email_sub = None
+                    for header in headers:
+                        if header['name'] == 'From':
+                            try:
+                                from_who = (re.findall(r'(<.*>)', header['value'])[0].replace("<", "").replace(">", ""))
+                            except IndexError:
+                                from_who = (header['value'])
 
-                    if header['name'] == 'Subject':
-                        email_sub = (header['value'])
-
-                if from_who and email_sub:
-                    email_list.append(Email_Object(message_id['id'], from_who, email_sub))
-                elif from_who:
-                    email_list.append(Email_Object(message_id['id'], from_who, ''))
-                elif email_sub:
-                    email_list.append(Email_Object(message_id['id'], '', email_sub))
-
-                # print("*********")
+                        if header['name'] == 'Subject':
+                            email_sub = (header['value'])
+                    try:
+                        if from_who and email_sub:
+                            email_list.append(Email_Object(message_id['id'], from_who, email_sub))
+                        elif from_who:
+                            email_list.append(Email_Object(message_id['id'], from_who, ''))
+                        elif email_sub:
+                            email_list.append(Email_Object(message_id['id'], '', email_sub))
+                    except KeyError, err:
+                        print ("Issue with message ID: " + message_id['id'])
+                        print ('Error: %s' % err)
+                except KeyError, error:
+                    print('Issue with message ID: ' + message_id['id'])
+                    print('Error: %s' % error)
+                
         
 def create_msg_labels(add_list, remove_list):
   """Create object to update labels.
+  Args:
+    add_list: List of labels that needs to be applied to a message
+    remove_list: List of labels that needs to be removed from a message
 
   Returns:
     A label update object.
@@ -129,8 +140,7 @@ def modify_message_labels(service, user_id, msg_id, msg_labels):
 
   Args:
     service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
+    user_id: User's email address. The special value "me" can be used to indicate the authenticated user.
     msg_id: The id of the message required.
     msg_labels: The change in labels.
 
@@ -138,10 +148,9 @@ def modify_message_labels(service, user_id, msg_id, msg_labels):
     Modified message, containing updated labelIds, id and threadId.
   """
   try:
-    message = service.users().messages().modify(userId=user_id, id=msg_id,
-                                                body=msg_labels).execute()
+    message = service.users().messages().modify(userId=user_id, id=msg_id, body=msg_labels).execute()
 
-    print ('Label changes for message with id: %s: ' % msg_labels)
+    print ('Label changes for message with id ' + msg_id + ': %s' % msg_labels)
 
     return message
   except errors.HttpError, error:
@@ -153,8 +162,7 @@ def delete_message(service, user_id, message_id):
 
   Args:
     service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
+    user_id: User's email address. The special value "me" can be used to indicate the authenticated user.
     msg_id: ID of Message to delete.
   """
   try:
@@ -164,6 +172,12 @@ def delete_message(service, user_id, message_id):
     print ('An error occurred: %s' % error)
 
 def create_label_mapping(service, user_id):
+    """Create a map of label names to label id that google recognizes 
+    Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special "me"
+        can be used to indicate the authenticated user.
+    """
     results = service.users().labels().list(userId=user_id).execute()
     labels = results.get('labels', [])
     if not labels:
@@ -198,10 +212,8 @@ def get_credentials():
 
 
 def main():
-    """Shows basic usage of the Gmail API.
-
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
+    """Accessing your Gmail via script.
+    You can do varaiety of things.
     """
 
     parser = argparse.ArgumentParser('gmail')
@@ -218,14 +230,13 @@ def main():
                         default='emails.csv')
     parser.add_argument('--include-subject',
                         action='store_true',
-                        help=('Include subject in output. '
-                              '(default: %(default)s))'),
-                        default='store_false')
+                        dest='include_subject',
+                        help=('Include subject in output.'),
+                        default=False)
     parser.add_argument('--read-file',
                         help=('Read file with email actions attached. '
                               '(must be a csv file created by this program default: %(default)s))'))
     args = parser.parse_args()
-
 
     email_counted = []
     user_id = 'me'
@@ -233,30 +244,37 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
     input_file = ''
-    columns = ["Message ID", "Email", "Count", "Action"]
+    columns = ["Message ID", "Email", "Count"]
 
     create_label_mapping(service, user_id)
 
     if args.include_subject:
         columns.append('Subject')
+        columns.remove('Count')
+    else:
+        columns.remove('Message ID')
 
     if args.read_file:
         print ('read file: %s' % args.read_file)
         readFile=csv.reader(open(args.read_file,'rU'), delimiter=',')
+        add_label_list = []
+        remove_label_list = []
+        subject_included = False
         
         for line in readFile:
-            message_id=line[0]
-            email=line[1]
-            action=line[3]
-            subject=line[4]
-            add_label_list = []
-            remove_label_list = []
-
-            search_query = 'from:' + email + ' and subject:' + subject
-
-            if email == "Email": #its the first line
+            if 'Subject' in line:
+                subject_included=True 
                 continue
 
+            if subject_included:
+                message_id=line[0]
+                email=line[1]
+                subject=line[2]
+                action=line[3]
+            else:
+                email=line[0]
+                action=line[2]
+                
             if action == 'delete':
                 delete_message(service, user_id, message_id)
             elif 'label' in action:
@@ -283,30 +301,40 @@ def main():
 
         thread_list = []
         if 'messages' in messages_response:
-            thread = appendList(user_id, messages_response['messages'])
+            message_id_list = messages_response['messages']
+            thread = processEmails(user_id, message_id_list)
+            thread.daemon = True
             thread_list.append(thread)
             thread.start()
 
         while 'nextPageToken' in messages_response:
             page_token = messages_response['nextPageToken']
             messages_response = service.users().messages().list(userId=user_id,q=args.query,pageToken=page_token).execute()
-            thread = appendList(user_id, messages_response['messages'])
-            thread_list.append(thread)
-            thread.start()
+            if 'messages' in messages_response:
+                message_id_list = messages_response['messages']
+                thread = processEmails(user_id, message_id_list)
+                thread.daemon = True
+                thread_list.append(thread)
+                thread.start()
 
-        for t in thread_list:
-            while t.isAlive():
-                print ('Checking email #' + str(len(email_list)+1), end='')
-                print ('\r', end='')
-                sys.stdout.flush()
+        thread_done_counter = 0
+        
+        while threading.active_count() != 1:
+            try:
+                print('Total threads done %s out of %s' % (len(thread_list) - (threading.active_count() - 1), len(thread_list)))
+                time.sleep(1)
+            except KeyboardInterrupt:
+                print "\nGoodbye!"
+                sys.exit() # Kill all threads
+        print('Total threads done %s out of %s' % (len(thread_list) - (threading.active_count() - 1), len(thread_list)))
 
         if args.include_subject:
             for entry in email_list:
-                writer.write(entry.message_id, entry.email_from, '1', '', entry.subject.encode('utf-8','ignore'))
+                writer.write(entry.message_id, entry.email_from, entry.subject.encode('utf-8','ignore'))
         else:
             counted_emails = collections.Counter(i.email_from for i in email_list)
             for entry in counted_emails.keys():
-                writer.write(entry, counted_emails[entry], '')
+                writer.write(entry, counted_emails[entry])
 
         writer.flush()
         print ('Total emails found: ' + str(len(email_list)))
